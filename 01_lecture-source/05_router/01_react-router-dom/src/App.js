b79a1aa95474ae751f3794ed16894dcafdc284fc
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2025-02-05 10:46:56 +0900

    [250205]

---
+++ 01_lecture-source/05_router/01_react-router-dom/src/App.js
@@ -0,0 +1,26 @@
+function App() {
+
+  /*
+    최초 프로젝트 기본 설정
+
+    - 프로젝트 만들고 싶은 폴더에 오른쪽 마우스 클릭하여 Open in integrated terminal
+     1. npx create-react-app ./
+    - 이후 기본 셋팅
+     2. src 폴더에 App.js, index.js 빼고 지우기!
+     3. App.js, index.js 에 불필요한 import 구문, 코드 지우기
+     4. npm install react-router-dom@6 으로 의존성 추가 
+
+    - 라우팅이란?
+     어떠한 요청을 어디로 안내 및 매핑할 것인지를 정해놓고 진행하는 것이다.
+     리액트는 요청에 따라 요청에 매핑 된 컴포넌트를 렌더링한다.
+     [ex] /menu/detail => <Detail/> 보여줄 주소와 컴포넌트를 매칭
+  */
+
+  return (
+    <div>
+      <h1>리엑트 하위~</h1>
+    </div>
+  );
+}
+
+export default App;
