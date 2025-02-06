10c963d206f425a43bd162a1d101f3a8b3450dec
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2025-02-06 10:43:43 +0900

    [250206]

--- 01_lecture-source/05_router/04_etc/src/pages/Mypage.js
+++ 01_lecture-source/05_router/04_etc/src/pages/Mypage.js
@@ -3,9 +3,20 @@ import {Navigate} from 'react-router-dom';
 /*
     컴포넌트 내에서 권한이나 다른 특정한 상황에 의해
     다른 페이지로 보내고 싶을 때 Navigate 라는 컴포넌트를 사용 할 수 있다.
+    useNavigate 랑 동일한 역할을 하지만 함수로 작성할 것인지 
+    컴포넌트로 작성할 것인지에 따라 선택 할 수 있다.
 */
 
 function Mypage() {
+
+    // 이런식으로 1. 로그인 유무에 따라 페이지를 정할수도있고
+    // 2. 권한에 따라 보여줄 페이지를 정할수도있고
+    // 3. 상태코드에 따라 에러페이지를 정할 수 있다
+    const isLogin = false;
+
+    if(!isLogin) {
+        return <Navigate to='/menu'/>;
+    }
 
     return(
         <div>
